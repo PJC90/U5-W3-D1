5c2043189c9341cbc0df6390172e6ec2cf0b7054
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2024-01-22 15:58:17 +0100

    createToken

--- src/main/java/pierpaolo/colasante/u5w2d5project/security/JWTtools.java
+++ src/main/java/pierpaolo/colasante/u5w2d5project/security/JWTtools.java
@@ -1,4 +1,23 @@
 package pierpaolo.colasante.u5w2d5project.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import pierpaolo.colasante.u5w2d5project.entities.User;
+
+import java.util.Date;
+
 public class JWTtools {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+    public String createToken(User user){
+        return Jwts.builder().subject(String.valueOf(user.getId())) // Subject <-- A chi appartiene il token (id dell'utente)
+                .issuedAt(new Date(System.currentTimeMillis())) // Data di emissione (IAT - Issued At)
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // Data di scadenza (Expiration Date)
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes())) // Firmo il token
+                .compact();
+    }
+    public void verifyToken(){
+
+    }
 }
--- src/main/java/pierpaolo/colasante/u5w2d5project/services/AuthService.java
+++ src/main/java/pierpaolo/colasante/u5w2d5project/services/AuthService.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Service;
 import pierpaolo.colasante.u5w2d5project.entities.User;
 import pierpaolo.colasante.u5w2d5project.exceptions.UnauthorizedException;
 import pierpaolo.colasante.u5w2d5project.payloads.UserLoginDTO;
+import pierpaolo.colasante.u5w2d5project.security.JWTtools;
 
 @Service
 public class AuthService {
     @Autowired
-    UserService userService;
+    private UserService userService;
+    @Autowired
+    private JWTtools jwTtools;
     @Autowired
     public String authenticateUser(UserLoginDTO body){
         // 1. Verifichiamo che l'email dell'utente sia nel db
@@ -17,7 +20,7 @@ public class AuthService {
         // 2. In caso affermativo, verifichiamo se la password fornita corrisponde a quella trovata nel db
         if(body.password().equals(user.getPassword())){
             // 3. Se le credenziali sono OK --> Genere un token JWT e lo ritorno
-            return "token appena c'è l'abbiamo";
+            return jwTtools.createToken(user);
 
         }else{
             // 4. Se le credenziali NON sono OK --> 401 (Unauthorized)
